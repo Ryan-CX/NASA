664db6ea45d88af59fb89a6f4cbaffdb511c5ae2
Chenguang Xu <xcg1234@gmail.com>
2022-01-06 23:12:09 -0500

    changed logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import "./styles.css";
-import logo from "../pic/logo.png";
 
 const Footer = () => {
 	return (
 		<div className="footer-div">
 			<div className="footer-logo-div">
-				<img className="footer-logo" src={logo} alt="NASA Logo" />
+				<img
+					className="footer-logo"
+					src={
+						"https://rawcdn.githack.com/Ryan-CX/Nasa/27d185ddb8d7651c6c2d63d94bb8eae4973b3c3c/src/pic/logo.png"
+					}
+					alt="NASA Logo"
+				/>
 			</div>
 			<div className="footer-contact">
 				<p>National Aeronautics and Space Administration</p>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 
 import "./styles.css";
-import logo from "../pic/logo.png";
 
 const Header = () => {
 	return (
 		<div className="header-div">
-			<img className="header-logo" src={logo} alt="NASA Logo" />
+			<img
+				className="header-logo"
+				src={
+					"https://rawcdn.githack.com/Ryan-CX/Nasa/27d185ddb8d7651c6c2d63d94bb8eae4973b3c3c/src/pic/logo.png"
+				}
+				alt="NASA Logo"
+			/>
 			<h1 className="title">Astronomy Photo of the Day</h1>
 		</div>
 	);
