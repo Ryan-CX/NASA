3435fd2fe6f1e440cb428a8565163d20cdf6f46a
Ryan (Chenguang) Xu <xcg1234@gmail.com>
2022-01-07 12:07:13 -0500

    add loading status

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart as faHeartSolid } from "@fortawesome/free-solid-svg-icons";
-import { faHeart as faHeartRegular } from "@fortawesome/free-regular-svg-icons";
+import React from 'react';
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart as faHeartSolid } from '@fortawesome/free-solid-svg-icons';
+import { faHeart as faHeartRegular } from '@fortawesome/free-regular-svg-icons';
 
-import { Badge } from "reactstrap";
-import styled from "styled-components";
-import "./styles.css";
+import { Badge } from 'reactstrap';
+import styled from 'styled-components';
+import './styles.css';
 
 // use styled component to create custom css wrapper
 const Container = styled.div`
@@ -20,7 +20,8 @@ const Container = styled.div`
 const NasaPhotoCard = ({ url, title, copyright, desc, date }) => {
 	const [like, setLike] = useState(false);
 
-	// to control the like button on/off
+	// to control the like button on/off, eventually if a users have their own account
+	//then we can set up a database to store the users' like status. But for now we just use a state to control the like button.
 	const handleClick = () => {
 		if (like === false) {
 			setLike(true);
@@ -30,35 +31,32 @@ const NasaPhotoCard = ({ url, title, copyright, desc, date }) => {
 	};
 
 	return (
-		<div className="photo-card">
+		<div className='photo-card'>
 			{/* The main image */}
-			<div className="photo-of-the-day-div">
-				<img className="photo-of-the-day" src={url} alt="APOD" />
+			<div className='photo-of-the-day-div'>
+				<img className='photo-of-the-day' src={url} alt='APOD' />
 			</div>
 
 			{/* Title and description */}
-			<div className="card-details">
+			<div className='card-details'>
 				<h2>{title} </h2>
-				<p className="copyright">
+				<p className='copyright'>
 					Image Credit and Copyright: {copyright}
-					<button className="like" onClick={handleClick}>
-						{/* simply use a flag to control the like button status */}
-						{like === true ? (
-							<FontAwesomeIcon
-								icon={faHeartSolid}></FontAwesomeIcon>
-						) : (
-							<FontAwesomeIcon
-								icon={faHeartRegular}></FontAwesomeIcon>
-						)}
+					<button className='like' onClick={handleClick}>
+						{/* simply use a flag to control the like button status,save the status to localStorage */}
+						<FontAwesomeIcon
+							icon={like ? faHeartSolid : faHeartRegular}
+							onClick={handleClick}
+						/>
 					</button>
 				</p>
 
-				<p className="card-content">{desc} </p>
+				<p className='card-content'>{desc} </p>
 			</div>
 
 			<Container>
-				<Badge className="badge">Date: {date} </Badge>
-				<Badge className="badge">Content and Image Credit: NASA</Badge>
+				<Badge className='badge'>Date: {date} </Badge>
+				<Badge className='badge'>Content and Image Credit: NASA</Badge>
 			</Container>
 		</div>
 	);
--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -1,35 +1,62 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import Card from "./Card";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Card from './Card';
+import ClipLoader from 'react-spinners/ClipLoader';
 
 function NasaPhotoData() {
 	const [nasaPhoto, setNasaPhoto] = useState([]);
 
+	//set the loading status to false initially
+	const [loading, setLoading] = useState(false);
+
+	//in order to see the loading status, I intentionally set a 1 second delay to show the loading status
+	//otherwise the loading will be too fast to see.
 	useEffect(() => {
-		axios
-			.get(
-				"https://api.nasa.gov/planetary/apod?api_key=eYhzAEYOjAYwGXFNdPhyNOycllNSCMzvEKMq00v1"
-			)
-			.then((response) => {
-				setNasaPhoto(response.data);
-			})
-			.catch((error) => {
-				console.log("there was an error", error);
-			});
+		//set the loading status to true
+		setLoading(true);
+		setTimeout(() => {
+			try {
+				async function getData() {
+					const res = await axios.get(
+						'https://api.nasa.gov/planetary/apod?api_key=eYhzAEYOjAYwGXFNdPhyNOycllNSCMzvEKMq00v1'
+					);
+					const data = await res.data;
+
+					setNasaPhoto(data);
+				}
+				getData();
+			} catch (error) {
+				console.log('there was an error', error);
+			}
+			//set the loading status to false
+			setLoading(false);
+		}, 1000);
 	}, []);
 
-	return (
-		<div className="photo-data-container">
-			<Card
-				key={nasaPhoto.id}
-				date={nasaPhoto.date}
-				title={nasaPhoto.title}
-				desc={nasaPhoto.explanation}
-				url={nasaPhoto.hdurl}
-				copyright={nasaPhoto.copyright}
+	//show loading status if loading
+	if (loading) {
+		return (
+			<ClipLoader
+				size={550}
+				color={'#F5A623'}
+				loading={loading}
+				speedMultiplier={1.5}
 			/>
-		</div>
-	);
+		);
+	} else {
+		return (
+			<div className='photo-data-container'>
+				<Card
+					key={nasaPhoto.id}
+					date={nasaPhoto.date}
+					title={nasaPhoto.title}
+					desc={nasaPhoto.explanation}
+					url={nasaPhoto.hdurl}
+					copyright={nasaPhoto.copyright}
+				/>
+			</div>
+		);
+	}
 }
 
 export default NasaPhotoData;
